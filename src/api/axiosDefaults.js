aa636238ecfb12b536d52fa8e23e0de0ae8f22b2
Kate Llewellyn <llewellyn.ksj@gmail.com>
2023-10-23 06:53:31 +0000

    Add hook

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://react-wt-e10c60ca8ff7.herokuapp.com/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
